c79c657ab09511f23ebc15311f82a95a07b894ab
Cesar Freitez <cesar.freitez@chepss.cl>
2022-09-28 21:33:32 +0000

    [ADD] Academy basic module

---
+++ chepss_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Chepss Academy',
+    'sumary': """Chepss Academy module""",
+    'description':"""
+        Chepss Academy to manage training:
+        - Training
+        - Session
+        - Attendees
+    """,
+    'author': 'CHEPSS',
+    'website': 'https://www.chepss.cl',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'license': 'LGPL-3',
+    'data':[
+        
+    ],
+    'demo':[
+        
+    ],
+}
