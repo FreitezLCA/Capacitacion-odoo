317440c3292b0ab04bbc05b47c335d3e773500cf
Cesar Freitez <cesar.freitez@chepss.cl>
2022-10-05 23:25:04 +0000

    [ADD] Esign document module

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Chepss Esign',
+    'sumary': """Chepss esign module""",
+    'description':"""
+        Chepss esign to manage documents:
+        - Esign
+        - Documents
+    """,
+    'author': 'CHEPSS',
+    'website': 'https://www.chepss.cl',
+    'category': 'Sign',
+    'version': '0.1',
+    'depends': ['hr'],
+    'license': 'LGPL-3',
+    'data':[
+        
+    ],
+    'demo':[
+        
+    ],
+}
